89dfb0bc76f0347f5585d57731373c219cbdbce9
wangyunchang <360812146@qq.com>
2023-07-01 22:15:30 +0800

    fix:漏采集问题+update:可自定义采集起始试卷

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 import re
+import sqlite3
 import time
 
 from docx import Document
@@ -9,9 +10,9 @@ import requests
 
 
 def get_exam_id(subject_id, t):
-    a = requests.get(
-        'https://www.zlketang.com/wxpub/api/simulationv4?from=web&channel=web&devtype=web&platform_type=web&subject_id={}&t={}'.format(
-            subject_id, t))
+    url = 'https://www.zlketang.com/wxpub/api/simulationv4?from=web&channel=web&devtype=web&platform_type=web&subject_id={}&t={}'.format(
+        subject_id, t)
+    a = requests.get(url=url, headers=header)
     exam_id_list = []
     if a.status_code == 200:
         response = json.loads(a.text)
@@ -27,7 +28,111 @@ def get_exam_id(subject_id, t):
     return exam_id_list
 
 
-def get_exam_detail(exam_id_list, cookie, referer):
+def get_exam_detail(exam_id_list, exam_chinese_name):
+    is_arrive = False
+    for exam_id in exam_id_list:
+        print(exam_id)
+        url = 'https://www.zlketang.com/wxpub/api/exam_question?exam_id={}&devtype=web'.format(exam_id)
+        a = requests.get(url, headers=header)
+        if a.status_code == 200:
+            response = json.loads(a.text)
+            exam_name = response['data']['exam_name']
+            try:
+                if not is_arrive:
+                    if exam_chinese_name == exam_name:
+                        is_arrive = True
+                    else:
+                        is_arrive = False
+                if is_arrive:
+                    doc = Document()
+                    doc.add_paragraph('试卷名称：' + exam_name)
+                    print(exam_name)
+                    detail = response['data']['parts']
+                    for per_part in detail:
+                        part_title = per_part['title']
+                        part_description = per_part['description']
+                        part_title_all = '{}（{}）'.format(part_title, part_description)
+                        doc.add_paragraph(part_title_all)
+                        if '单项选择题' in part_title or '判断题' in part_title:
+                            doc.add_paragraph(f'<TYPE.TAG>单项选择题')
+                        elif '多项选择题' in part_title:
+                            doc.add_paragraph(f'<TYPE.TAG>多项选择题')
+                        else:
+                            doc.add_paragraph(f'<TYPE.TAG>文字题')
+                        print(part_title_all)
+                        questions = per_part['questions']
+                        i = 1
+                        for question in questions:
+                            clean = re.compile('<.*?>')
+                            question_title = question['description']
+                            question_title = re.sub(clean, '', question_title)
+                            question_title = f'{i}、' + question_title
+                            doc.add_paragraph(question_title)
+                            print(question_title)
+                            options = question['options']
+                            options = json.loads(re.sub(clean, '', options))
+                            if '计算' in part_title or '综合题' in part_title or '简答题' in part_title:
+                                j = 1
+                                if type(options) == list:
+                                    for per_trouble in options:
+                                        trouble_title = f'（{j}）' + per_trouble['description']
+                                        j = j + 1
+                                        print(trouble_title)
+                                        doc.add_paragraph(trouble_title)
+                                else:
+                                    doc.add_paragraph('')
+                                answer = question['answer']
+                                if '[' in answer:
+                                    answers = json.loads(answer)
+                                    doc.add_paragraph('参考答案:')
+                                    for answer in answers:
+                                        doc.add_paragraph(answer)
+                                        print(answer)
+                                else:
+                                    doc.add_paragraph(answer)
+                                solution = question['solution']
+                                solution = re.sub(clean, '', solution)
+                                if '[' in solution:
+                                    solutions = json.loads(solution)
+                                    doc.add_paragraph('解析:')
+                                    for solution in solutions:
+                                        doc.add_paragraph(solution)
+                                        print(solution)
+                                    doc.add_paragraph('分数:1')
+                                else:
+                                    doc.add_paragraph(solution)
+                                    doc.add_paragraph('分数:1')
+                                i = 1 + i
+                            else:
+                                for k, v in options.items():
+                                    option = '{}.{}'.format(k, v)
+                                    doc.add_paragraph(option)
+                                    print(option)
+                                answer = question['answer'].replace(',', '')
+                                answer = '参考答案:{}'.format(answer)
+                                doc.add_paragraph(answer)
+                                print(answer)
+                                solution = question['solution']
+                                solution = re.sub(clean, '', solution)
+                                solution = '解析:{}'.format(solution)
+                                doc.add_paragraph(solution)
+                                doc.add_paragraph('分数:1')
+                                print(solution)
+                                i = 1 + i
+                    doc.save("{}.docx".format(exam_name))
+                    time.sleep(3)
+            except Exception as e:
+                print('报错如下：{}，可以尝试换cookie重启程序，如不行再联系开发者！'.format(e))
+                break
+        else:
+            print('网络问题or反爬，采集失败！')
+
+
+if __name__ == '__main__':
+    subject_id = input('请输入参数subject_id：')
+    t = input('请输入参数t：')
+    cookie = input('请输入cookie：')
+    referer = input('请输入referer：')
     header = {
         "Accept": "*/*",
         "Accept-Encoding": "gzip, deflate, br",
@@ -43,108 +148,12 @@ def get_exam_detail(exam_id_list, cookie, referer):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
         "X-Requested-With": "XMLHttpRequest"
     }
-    for exam_id in exam_id_list:
-        print(exam_id)
-        url = 'https://www.zlketang.com/wxpub/api/exam_question?exam_id={}&devtype=web'.format(exam_id)
-        a = requests.get(url, headers=header)
-        if a.status_code == 200:
-            response = json.loads(a.text)
-            try:
-                exam_name = response['data']['exam_name']
-                doc = Document()
-                doc.add_paragraph('试卷名称：' + exam_name)
-                print(exam_name)
-                detail = response['data']['parts']
-                for per_part in detail:
-                    part_title = per_part['title']
-                    part_description = per_part['description']
-                    part_title_all = '{}（{}）'.format(part_title, part_description)
-                    doc.add_paragraph(part_title_all)
-                    if '单项选择题' in part_title or '判断题' in part_title:
-                        doc.add_paragraph(f'<TYPE.TAG>单项选择题')
-                    elif '多项选择题' in part_title:
-                        doc.add_paragraph(f'<TYPE.TAG>多项选择题')
-                    else:
-                        doc.add_paragraph(f'<TYPE.TAG>文字题')
-                    print(part_title_all)
-                    questions = per_part['questions']
-                    i = 1
-                    for question in questions:
-                        clean = re.compile('<.*?>')
-                        question_title = question['description']
-                        question_title = re.sub(clean, '', question_title)
-                        question_title = f'{i}、' + question_title
-                        doc.add_paragraph(question_title)
-                        print(question_title)
-                        options = question['options']
-                        options = json.loads(re.sub(clean, '', options))
-                        if '计算' in part_title or '综合题' in part_title or '简答题' in part_title:
-                            j = 1
-                            if type(options) == list:
-                                for per_trouble in options:
-                                    trouble_title = f'（{j}）' + per_trouble['description']
-                                    j = j + 1
-                                    print(trouble_title)
-                                    doc.add_paragraph(trouble_title)
-                            else:
-                                doc.add_paragraph('')
-                            answer = question['answer']
-                            if '[' in answer:
-                                answers = json.loads(answer)
-                                doc.add_paragraph('参考答案:')
-                                for answer in answers:
-                                    doc.add_paragraph(answer)
-                                    print(answer)
-                            else:
-                                doc.add_paragraph(answer)
-                            solution = question['solution']
-                            solution = re.sub(clean, '', solution)
-                            if '[' in solution:
-                                solutions = json.loads(solution)
-                                doc.add_paragraph('解析:')
-                                for solution in solutions:
-                                    doc.add_paragraph(solution)
-                                    print(solution)
-                                doc.add_paragraph('分数:1')
-                            else:
-                                doc.add_paragraph(solution)
-                                doc.add_paragraph('分数:1')
-                            i = 1 + i
-                        else:
-                            for k, v in options.items():
-                                option = '{}.{}'.format(k, v)
-                                doc.add_paragraph(option)
-                                print(option)
-                            answer = question['answer'].replace(',', '')
-                            answer = '参考答案:{}'.format(answer)
-                            doc.add_paragraph(answer)
-                            print(answer)
-                            solution = question['solution']
-                            solution = re.sub(clean, '', solution)
-                            solution = '解析:{}'.format(solution)
-                            doc.add_paragraph(solution)
-                            doc.add_paragraph('分数:1')
-                            print(solution)
-                            i = 1 + i
-                doc.save("{}.docx".format(exam_name))
-                time.sleep(3)
-            except Exception as e:
-                print('报错如下：{}'.format(e))
-        else:
-            print('网络问题or反爬，采集失败！')
-
-
-if __name__ == '__main__':
-    subject_id = input('请输入参数subject_id：')
-    t = input('请输入参数t：')
     a = get_exam_id(subject_id, t)
-    print(a)
     if a == '网络问题or反爬，采集失败！':
         print('采集失败，已结束程序！！！')
         pass
     else:
+        exam_chinese_name = input('请输入想要开始采集的试卷名：')
         print('考卷id：{}'.format(a))
-        cookie = input('请输入cookie：')
-        referer = input('referer：')
-        get_exam_detail(a, cookie, referer)
+        get_exam_detail(a, exam_chinese_name)
         print('===========程序结束！！==========')
